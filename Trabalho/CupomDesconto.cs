abddac609f0f4a8945d0f1e15663db842f2f4d36
João Gabriel <163910348+Joaonzin@users.noreply.github.com>
2024-07-15 16:55:58 -0300

    x

--- Trabalho/CupomDesconto.cs
+++ Trabalho/CupomDesconto.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Trabalho
 {
-    internal class CupomDesconto
+    public class CupomDesconto
     {
+        public string Codigo { get; set; }
+        public double Desconto { get; set; } // Representa o desconto em porcentagem
+
+        public CupomDesconto(string codigo, double desconto)
+        {
+            Codigo = codigo;
+            Desconto = desconto;
+        }
     }
 }
